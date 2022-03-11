65ac2fb0eb9f063faa013c81373b08734217d78d
Barbara Dainese <barbara.dainese@venturus.org.br>
2022-03-11 10:25:56 -0300

    atualizando exercício

---
+++ pratica_01/script.js
@@ -0,0 +1,10 @@
+//função entrar
+
+function enter(){
+    var area = document.getElementById('area');
+    var texto = prompt('digite seu nome:');
+
+    area.innerHTML = 'bem vindo' + texto;
+}
+
+
