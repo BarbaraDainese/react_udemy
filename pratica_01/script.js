68eff3309e62330ba09c51739e430cdb1924a32a
Barbara Dainese <barbara.dainese@venturus.org.br>
2022-03-11 10:41:50 -0300

    atualizando

--- pratica_01/script.js
+++ pratica_01/script.js
@@ -4,7 +4,20 @@ function enter(){
     var area = document.getElementById('area');
     var texto = prompt('digite seu nome:');
 
-    area.innerHTML = 'bem vindo' + texto;
+    if(texto == '' || texto == null){
+        alert('Digite seu nome.');
+        area.innerHTML = 'Bem vindo...';
+
+    } else{
+        area.innerHTML = 'Bem vindo ' + texto;
+    }
+}
+
+function enter2(nome){
+    var area = document.getElementById('area2');
+    var texto = prompt('digite seu sobrenome:');
+    area.innerHTML = nome + " " + texto;
+
 }
 
 
