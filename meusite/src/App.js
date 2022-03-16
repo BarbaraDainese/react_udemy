e68e62a1edcfe80a32733b9ccf5be79b016d6a96
Barbara Dainese <barbara.dainese@venturus.org.br>
2022-03-16 10:55:50 -0300

    atualizando state

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,44 +1,42 @@
-import { render } from '@testing-library/react';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Equipe extends Component{
-    render(){
-        return(
-            <><Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade} /><Social /><hr /></>
-        );
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state= {
+                nome:'Barbara',
+                contador: 0
+        };
+
+        this.aumentar = this.aumentar.bind(this);
+        this.diminuir = this.diminuir.bind(this);
     }
-}
 
-class Sobre extends Component{
+    aumentar(){
+        let state = this.state;
+        state.contador += 1;
+        this.setState(state)
+    }
+
+    diminuir(){
+        let state = this.state;
+        state.contador -= 1;
+        this.setState(state)
+    }
+
     render(){
         return(
             <div>
-                <h2>Olá! Sou o (a) {this.props.nome}</h2>
-                <h3>Cargo: {this.props.cargo}</h3>
-                <h3>Idade: {this.props.idade}</h3>
+            <h1>Contador</h1>
+                <h3>
+                <button onClick={this.diminuir}>-</button>
+                {this.state.contador}
+                <button onClick={this.aumentar}>+</button>
+                </h3>
             </div>
         );
     }
-}
-
-const Social = () =>{
-    return(
-        <div>
-        <a>Facebook </a>
-        <a>LinkedIn </a>
-        </div>
-        );
-}
-
-function App(){
-    return(
-        <div>
-            <h1>Conheça nossa equipe:</h1>
-            <Equipe nome="Matheus" cargo="Programador" idade="30"/>
-            <Equipe nome="Maria" cargo="Designer" idade="32"/>
-            <Equipe nome="Julio" cargo="Frontend" idade="25"/>
-        </div>
-    );
 }
 
 export default App;
