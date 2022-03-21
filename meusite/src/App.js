4412c59f310d9bb2409a2bc1c6702457652917d6
Barbara Dainese <barbara.dainese@venturus.org.br>
2022-03-21 10:13:33 -0300

    atualizando

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -4,17 +4,46 @@ class App extends Component{
     constructor(props){
         super(props);
         this.state = {
+            email: 'teste@teste.com',
+            senha:'123456',
+            sexo: 'Masculino'
 
         }
+
+        this.trocaEmail = this.trocaEmail.bind(this);
+        this.trocaSexo = this.trocaSexo.bind(this);
+
+     
+
     }
+
+    trocaEmail(event){
+        let valorDigitado = event.target.value;
+
+        this.setState({email:valorDigitado});
+    }
+
+    trocaSexo(event){
+        let valorDigitado = event.target.value;
+        this.setState({sexo: valorDigitado});
+    }
+
+    
 
     render(){
         return(
             <div>
                 Email:
-                <input type='email' name="email" value="teste@teste.com"></input><br/>
+                <input type='email' name="email" value={this.state.email}
+                onChange={this.trocaEmail}></input><br/>
                 Senha:
-                <input type='password' name="senha" value="1234"></input>
+                <input type='password' name="senha" value={this.state.senha}
+                onChange={(e)=> this.setState({senha:e.target.value})}/><br/>
+                Sexo:
+                <select name="sexo" value={this.state.sexo} onChange={this.trocaSexo}>
+                    <option value="masculino">Masculino</option>
+                    <option value="Feminino">Feminino</option>
+                </select>
             </div>
         )
     }
